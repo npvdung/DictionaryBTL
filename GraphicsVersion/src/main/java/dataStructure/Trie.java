8f1aa3e7ec7484f6915be699bf0db2ec3377373a
lechiennn <79262060+lechiennn@users.noreply.github.com>
2022-07-31 21:37:23 +0700

    create fxml prj

---
+++ GraphicsVersion/src/main/java/dataStructure/Trie.java
@@ -0,0 +1,53 @@
+package dataStructure;
+
+public class Trie {
+    public static TrieNode root = new TrieNode();
+
+    public void insert(String word) {
+        word = word.toLowerCase();
+        TrieNode trieNode = root;
+        for (int i = 0; i < word.length(); i++) {
+
+            int index = word.charAt(i) - 'a';
+
+            if (trieNode.getChildren()[index] == null) {
+                trieNode.getChildren()[index] = new TrieNode();
+            }
+            trieNode = trieNode.getChildren()[index];
+        }
+        trieNode.setEndOfWord(true);
+        trieNode.setWordData(word);
+    }
+
+    public TrieNode search(String prefix) {
+        prefix = prefix.toLowerCase();
+        TrieNode trieNode = root;
+        for (int i = 0; i < prefix.length(); i++) {
+            int index = prefix.charAt(i) - 'a';
+            if (trieNode.getChildren()[index] == null) {
+                return null;
+            }
+            trieNode = trieNode.getChildren()[index];
+        }
+        return trieNode;
+    }
+
+    public String find(TrieNode keyNode) {
+        if (keyNode == null) {
+            return "";
+        }
+        String ans = (keyNode.isEndOfWord() ? keyNode.getWordData() + "@" : "");
+        for (int i = 0; i < TrieNode.ALPHABET_SIZE; i++) {
+            if (keyNode.getChildren()[i] != null) {
+                ans = ans.concat(find(keyNode.getChildren()[i]));
+            }
+        }
+        return ans;
+    }
+
+    public String[] findAllWord(String key) {
+        key = key.toLowerCase();
+        TrieNode trieNode = this.search(key);
+        return this.find(trieNode).split("@");
+    }
+}
